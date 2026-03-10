632bed9c7a10fe97f3390109e437b3dd48466674
Matthew Estilo <matthewestilo2@gmail.com>
2026-03-10 22:42:14 +0800

    .

---
+++ app/components/AdBanner.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+interface AdBannerProps {
+  dataAdSlot: string;
+  dataAdFormat?: string;
+  dataFullWidthResponsive?: boolean;
+  style?: React.CSSProperties;
+}
+
+export default function AdBanner(_props: AdBannerProps) {
+  return null;
+}
